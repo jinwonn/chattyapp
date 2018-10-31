3f70410e322593e2b282b8a664972de70336a959
Daniel <d.t.nguyen@live.com>
2018-10-31 21:51:44 +0000

    implemented new mesage rendering

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {loading: true,
+                  currentmessageid: 5,
                   currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
                   messages: [
                     {
@@ -22,17 +23,51 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.setState({loading: false});
+    //simulate incoming message
     setTimeout(() => {
-      this.setState({loading: false});
-    }, 300)
-  }
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {key: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 1000);
+
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {key: 4, username: "Daniel", content: "Hello!"};
+    const messages = this.state.messages.concat(newMessage)
+    console.log(messages, "hardcoded")
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+  }, 2000);
+
+}
+
+_newMessage = (message) => {
+  let id = this.state.currentmessageid;
   
+  if (message.username) {
+    var newMessage = {username: message.username, content: message.message, key: id++}
+  } else {
+    var newMessage = {username: "Anonymous", content: message.message, key: id++}
+  }
+    const messages = this.state.messages.concat(newMessage)
+
+    this.setState({messages: messages, currentmessageid: id})
+    console.log(this.state)
+}
+ 
   render() {
     return (
       <div>
       <nav className="navbar"><a href="/" className="navbar-brand">Chatty</a></nav>
       <MessageList messages = {this.state.messages}/>
-      <ChatBar currentUser = {this.state.currentUser}/>
+      <ChatBar currentUser = {this.state.currentUser} onSend = {this._newMessage}/>
       </div>
     );
 
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,13 +3,30 @@ import React, {Component} from 'react';
 class ChatBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {message: ""} 
   }
 
+  _handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+    this.props.onSend(this.state)
+    this.setState({message: ""});
+    }
+  }
+  
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder={this.props.currentUser.name}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" value={this.state.username}
+           onChange={(e) => {
+             this.setState({username: e.target.value});
+            //  console.log(this.state)
+           }}/>
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" value={this.state.message} 
+           onChange={(e) => {
+             this.setState({message: e.target.value});
+            //  console.log(this.state)
+           }}
+           onKeyPress={this._handleKeyPress}/>
       </footer>
     );
   }
