65058e1ce63c61802d8399db36ba97606b9d1d1d
Daniel <d.t.nguyen@live.com>
2018-11-01 17:55:29 +0000

    set up server and client for client to send message to server

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,35 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.socket = new WebSocket('ws://0.0.0.0:3001/')
+    this.socket.onopen = (con) => {console.log('Connected to server')}
+    console.log(this.socket)
+
     this.setState({loading: false});
+    
     setTimeout(() => {
       const newMessage = {key: 1, username: "ChattyBot", content: "Hello there! Has anyone seen my marbles? Please enter your name and start chatting away!"};
       const messages = this.state.messages.concat(newMessage)
       this.setState({messages: messages})
     }, 1500);
 }
+  _sendMessage = (message) => {
+    this.socket.send(JSON.stringify(message))
+    console.log(this.socket)
+    console.log("sent", message)
 
-_newMessage = (message) => {
-  let id = this.state.currentmessageid;
-
-  if (message.username) {
-    var newMessage = {username: message.username, content: message.message, key: id++}
-  } else {
-    var newMessage = {username: "Anonymous", content: message.message, key: id++}
   }
-    const messages = this.state.messages.concat(newMessage)
 
+  _newMessage = (message) => {
+    let id = this.state.currentmessageid;
+    if (message.username) {
+        var newMessage = {username: message.username, content: message.message, key: id++}
+      } else {
+        var newMessage = {username: "Anonymous", content: message.message, key: id++}
+      }
+    const messages = this.state.messages.concat(newMessage)
     this.setState({messages: messages, currentmessageid: id})
-}
+  }
  
   render() {
     return (
@@ -40,7 +49,7 @@ _newMessage = (message) => {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages = {this.state.messages}/>
-        <ChatBar currentUser = {this.state.currentUser} onSend = {this._newMessage}/>
+        <ChatBar currentUser = {this.state.currentUser} onSend = {this._sendMessage}/>
       </div>
     );
 
