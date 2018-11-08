31f42f5d051bdbcc83d6038f677386fb453e245b
Daniel <d.t.nguyen@live.com>
2018-11-08 04:15:36 +0000

    seperated server from chatapp

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-const uuidv4 = require('uuid/v4');
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
@@ -18,69 +17,3 @@ new WebpackDevServer(webpack(config), {
 
     console.log('Running at http://0.0.0.0:3000');
   });
-
-
-  // server.js
-
-const express = require('express');
-const SocketServer = require('ws').Server;
-const WebSocket = require('ws');
-
-// Set the port to 3001
-const PORT = 3001;
-
-// Create a new express server
-const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
-
-// Create the WebSockets server
-const wss = new SocketServer({ server });
-
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
-  wss.on('connection', (ws) => {
-  console.log('Client connected');
-  updateUsersConnected();
-  
-
-  ws.on('message', (data) => {
-    let message = JSON.parse(data);
-    console.log("received", message);
-    if (message.type == "postMessage") {
-      message.id = uuidv4();
-      message.type = "incomingMessage"
-      if (!message.username) {
-        message.username = 'Anonymous';
-      }
-    } else if (message.type == "postNotification") {
-      message.id = uuidv4();
-      message.type = "incomingNotification"
-    }
-    sendMessage(message)
-  });
-  
-  function updateUsersConnected() {
-    usersconnected = {
-      type: 'incomingInfo',
-      content: wss.clients.size
-      }
-    sendMessage(usersconnected);
-  }
-  function sendMessage(message) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(message));
-        console.log("Server sent:", message);
-      } else { console.log("not connected", client.readyState, WebSocket.OPEN)}
-    });
-  }
-
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => {
-    console.log('Client disconnected')
-    updateUsersConnected();
-  });
-});
